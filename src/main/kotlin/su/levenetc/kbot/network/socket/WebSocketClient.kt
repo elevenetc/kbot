4d9c12ea7524e5d4e3850af8a2549591f7592be9
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-15 16:43:47 +0200

    Add netty wss client

---
+++ src/main/kotlin/su/levenetc/kbot/network/socket/WebSocketClient.kt
@@ -0,0 +1,76 @@
+package su.levenetc.kbot.network.socket
+
+import io.netty.bootstrap.Bootstrap
+import io.netty.buffer.ByteBuf
+import io.netty.channel.Channel
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioSocketChannel
+import io.netty.handler.codec.http.HttpClientCodec
+import io.netty.handler.codec.http.HttpHeaders
+import io.netty.handler.codec.http.HttpObjectAggregator
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory
+import io.netty.handler.codec.http.websocketx.WebSocketVersion
+import java.net.URI
+import java.util.*
+
+
+class WebSocketClient(val uri: String) {
+
+    lateinit var ch: Channel
+
+    fun connect() {
+        val bootstrap = Bootstrap()
+        val uri: URI = URI.create(uri)
+        val handler = WebSocketClientHandler(WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13, null, false, HttpHeaders.EMPTY_HEADERS, 1280000))
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel::class.java)
+                .handler(object : ChannelInitializer<SocketChannel>() {
+                    override fun initChannel(ch: SocketChannel) {
+                        val pipeline = ch.pipeline()
+                        pipeline.addLast("http-codec", HttpClientCodec())
+                        pipeline.addLast("aggregator", HttpObjectAggregator(65536))
+                        pipeline.addLast("ws-handler", handler)
+                    }
+                })
+        ch = bootstrap.connect(uri.host, 443).sync().channel()
+        handler.channelPromise.sync()
+    }
+
+    fun close() {
+        ch.writeAndFlush(CloseWebSocketFrame())
+        ch.closeFuture().sync()
+    }
+
+    fun eval(text: String) {
+        ch.writeAndFlush(TextWebSocketFrame(text))
+    }
+
+    companion object {
+        private val group = NioEventLoopGroup()
+    }
+
+    inner class TimeClientHandler : ChannelInboundHandlerAdapter() {
+        override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+            val m = msg as ByteBuf // (1)
+            try {
+                val currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L
+                System.out.println(Date(currentTimeMillis))
+                ctx.close()
+            } finally {
+                m.release()
+            }
+        }
+
+        override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
+            cause.printStackTrace()
+            ctx.close()
+        }
+    }
+}
---
+++ src/main/kotlin/su/levenetc/kbot/network/socket/WebSocketClientHandler.kt
@@ -0,0 +1,89 @@
+package su.levenetc.kbot.network.socket
+
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelPromise
+import io.netty.channel.SimpleChannelInboundHandler
+import io.netty.handler.codec.http.FullHttpResponse
+import io.netty.handler.codec.http.websocketx.*
+
+class WebSocketClientHandler(val handShaker: WebSocketClientHandshaker) : SimpleChannelInboundHandler<Any>() {
+
+    lateinit var channelPromise: ChannelPromise
+
+    override fun handlerAdded(ctx: ChannelHandlerContext) {
+        println("handleAdded")
+        channelPromise = ctx.newPromise()
+    }
+
+    override fun channelActive(ctx: ChannelHandlerContext) {
+        println("channelActive")
+        handShaker.handshake(ctx.channel())
+    }
+
+    override fun channelInactive(ctx: ChannelHandlerContext) {
+        println("channelInactive")
+    }
+
+    override fun userEventTriggered(ctx: ChannelHandlerContext?, evt: Any?) {
+        println("userEventTriggered")
+        super.userEventTriggered(ctx, evt)
+    }
+
+    override fun handlerRemoved(ctx: ChannelHandlerContext?) {
+        println("handlerRemoved")
+        super.handlerRemoved(ctx)
+    }
+
+    override fun channelRead0(ctx: ChannelHandlerContext, msg: Any) {
+        println("channelRead0: $msg")
+        val ch = ctx.channel()
+        if (!handShaker.isHandshakeComplete) {
+            handShaker.finishHandshake(ch, msg as FullHttpResponse)
+            channelPromise.setSuccess()
+            return
+        }
+
+        val frame = msg as WebSocketFrame
+        if (frame is TextWebSocketFrame) {
+            println("text message: $frame")
+        } else if (frame is PongWebSocketFrame) {
+            println("pont message")
+        } else if (frame is CloseWebSocketFrame) {
+            ch.close()
+        } else {
+            println("unhandled frame: $frame")
+        }
+    }
+
+    override fun channelRead(ctx: ChannelHandlerContext?, msg: Any?) {
+        println("channelRead: $msg")
+        super.channelRead(ctx, msg)
+    }
+
+    override fun acceptInboundMessage(msg: Any?): Boolean {
+        println("acceptInboundMessage: $msg")
+        return super.acceptInboundMessage(msg)
+    }
+
+    override fun channelUnregistered(ctx: ChannelHandlerContext?) {
+        println("channelUnregistered")
+        super.channelUnregistered(ctx)
+    }
+
+    override fun channelRegistered(ctx: ChannelHandlerContext?) {
+        println("channelRegistered")
+        super.channelRegistered(ctx)
+    }
+
+    override fun channelReadComplete(ctx: ChannelHandlerContext?) {
+        println("channelReadComplete")
+        super.channelReadComplete(ctx)
+    }
+
+    override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
+        println("exceptionCaught")
+        cause.printStackTrace()
+        if (!channelPromise.isDone) channelPromise.setFailure(cause)
+        ctx.close()
+    }
+}
